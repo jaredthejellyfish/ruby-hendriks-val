a5ea64efeb4489e4cc0d02d86f542d8950d58055
Gerard Hernandez <ger.almenara@gmail.com>
2022-11-03 17:51:38 +0100

    deleted:    test_generator.rb

--- test_generator.rb
+++
@@ -1,8 +0,0 @@
-inst_vars = %w[puuid region name tag account_level card last_update last_update_raw]
-
-inst_vars.each do |var|
-    puts "it '@#{var} should return a string' do"
-    puts "    expect(user.#{var}).to be_a(String)"
-    puts "end"
-    puts
-end
